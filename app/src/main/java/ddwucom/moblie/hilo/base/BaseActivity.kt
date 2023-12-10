86509674b6723416dcc9a77e9ebfb4e574ab86ec
Parkseojin <seojin5565@gmail.com>
2023-12-10 19:56:50 +0900

    [featear] : BaseActivity에 뒤로가기 버튼 두번 눌렀을 때 동작추가 #4

--- app/src/main/java/ddwucom/moblie/hilo/base/BaseActivity.kt
+++ app/src/main/java/ddwucom/moblie/hilo/base/BaseActivity.kt
@@ -1,6 +1,7 @@
 package ddwucom.moblie.hilo.base
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -8,9 +9,9 @@ import androidx.databinding.ViewDataBinding
 
 abstract class BaseActivity<T : ViewDataBinding>
 (@LayoutRes private val layoutId: Int) : AppCompatActivity() {
-
     private var _binding: T? = null
     val binding get() = _binding!!
+    private var waitTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +20,15 @@ abstract class BaseActivity<T : ViewDataBinding>
 
     override fun onDestroy() {
         super.onDestroy()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (System.currentTimeMillis() - waitTime >= 1500) {
+            waitTime = System.currentTimeMillis()
+            Toast.makeText(this, "뒤로가기 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            finish()
+        }
     }
 }
