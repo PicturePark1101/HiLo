6ed7ab63c3a816192d068c811950efac41ddeb55
Parkseojin <seojin5565@gmail.com>
2023-12-10 20:04:37 +0900

    [feat : BaseViewModel 추가 #4

---
+++ app/src/main/java/ddwucom/moblie/hilo/base/BaseViewModel.kt
@@ -0,0 +1,21 @@
+package ddwucom.moblie.hilo.base
+
+import android.view.View
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
+
+open class BaseViewModel : ViewModel(){
+
+    private val compositeDisposable = CompositeDisposable()
+
+    fun addDisposable(disposable: Disposable) {
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+        super.onCleared()
+    }
+}
